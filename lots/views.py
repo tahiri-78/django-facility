be460d442098d3206f7de8a3cea8692ab4b4df14
tahiri ahmed <tahiria@DATAEMBASSY.INT>
2022-11-24 12:11:50 +0100

    ajout de templates au projet

--- lots/views.py
+++ lots/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from .models import Lots
 
 def lot_list(request):
-    pass
+    lots_list=Lots.objects.all()
+    context={'lots':lots_list}
+    return render(request,'lots/liste_lots.html',context )
 
 
 def lot_detail(request,id):
-    pass
+    lot_list=Lots.objects.get(id=id)
+    context={'lots':lot_list}
+    return render(request,'lots/detail_lot.html',context )
+
   
